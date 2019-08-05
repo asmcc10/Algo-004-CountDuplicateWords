d0cb56824439c1f59a5ecd0b487b6be1442e3ab5
asmcc10 <asmcc10@morgan.edu>
2019-08-05 09:50:31 -0400

    Still need to reference input

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,22 @@
 package hbcu.stay.ready.algor;
 
+import java.util.*;
+
 public class Solution {
 
     public String countUniqueWords(String input){
-        return null;
+
+        //must find where to implement input, currently not pointing to anything
+        List<String> wordsArray = Arrays.asList(" ");
+
+        Set<String> uniqueWords = new HashSet<>(wordsArray);
+        String output = "The " + uniqueWords.size() + " unique words are:\n";
+        for(int x = 0; x < wordsArray.size(); x++){
+            String words = wordsArray.get(x);
+            output = output + words + " (Seen " + Collections.frequency(wordsArray, uniqueWords) +
+                    ")\n";
+        }
+        return output;
+
     }
 }
