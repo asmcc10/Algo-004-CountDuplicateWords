6e8034b6b5b44c27fd802e250dd93ac8bf8eb89c
asmcc10 <asmcc10@morgan.edu>
2019-08-05 21:13:42 -0400

    Still failed

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -6,15 +6,33 @@ public class Solution {
 
     public String countUniqueWords(String input){
 
-        //must find where to implement input, currently not pointing to anything
-        List<String> wordsArray = Arrays.asList(" ");
+        String replacePeriods = input.replaceAll(".", "");
+        String replaceExplamation = replacePeriods.replace("!", "");
+        String [] splitString = replaceExplamation.split(" ");
 
-        Set<String> uniqueWords = new HashSet<>(wordsArray);
+        Map<String, Integer> uniqueWords = new HashMap<>();
+
+        for(String word : splitString){
+            if(!uniqueWords.containsKey(word)){
+                uniqueWords.put(word, 1);
+                }
+            else {
+                int value = uniqueWords.get(word);
+                value++;
+                uniqueWords.put(word, value);
+            }
+        }
+
+        int size = 0;
         String output = "The " + uniqueWords.size() + " unique words are:\n";
-        for(int x = 0; x < wordsArray.size(); x++){
-            String words = wordsArray.get(x);
-            output = output + words + " (Seen " + Collections.frequency(wordsArray, uniqueWords) +
-                    ")\n";
+        for (String string : uniqueWords.keySet()) {
+            size++;
+            int val = uniqueWords.get(string);
+            if (size < uniqueWords.size())
+                output = output + uniqueWords.get(string) + " (Seen " + val + ")\n";
+            else
+                output = output + uniqueWords.get(string) + " (Seen " + val + ")";
+
         }
         return output;
 
